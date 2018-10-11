84de6daea27d30e56779fba605e1423722369b88
Narakky <11222997+Narakky@users.noreply.github.com>
2018-10-11 11:29:31 +0900

    made controllers

---
+++ RxExample/Controllers/Common/MainNavigationController.swift
@@ -0,0 +1,17 @@
+//
+//  MainNavigationController.swift
+//  RxExample
+//
+//  Created by Naraki on 10/11/18.
+//  Copyright © 2018 i-enter. All rights reserved.
+//
+
+import UIKit
+
+final class MainNavigationController: UINavigationController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
--- RxExample/Controllers/Main/MainViewController.swift
+++ RxExample/Controllers/Main/MainViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MainViewController.swift
 //  RxExample
 //
 //  Created by Naraki on 10/11/18.
@@ -8,13 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class MainViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
-
 }
-
